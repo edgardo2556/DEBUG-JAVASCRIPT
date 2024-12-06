d1fc63bf10562aa7f22656b1cb4fb2e45655a0dc
edgardo <jugador.eo@gmail.com>
2024-12-07 00:27:41 +0100

    Delete cambio.js

--- cambio.js
+++
@@ -1,5 +0,0 @@
-function cambiarColor() {
-  const colores = ["red", "blue", "green", "yellow", "purple"];
-  const indice = Math.floor(Math.random() * colores.length);
-  document.body.style.backgroundColor = colores[indice];
-}
