a52b31a9e68846a1383d99670f18590f072caa39
edgardo <jugador.eo@gmail.com>
2024-12-07 22:51:30 +0100

    validador

---
+++ validaroformulario/validador.js
@@ -0,0 +1,29 @@
+
+ function validarFormulario(){
+   // event.preventDefault();
+ if(validarFormulario2()){
+  alert("Formulario correcto!");
+  }else{
+   alert("Formulario incorrecto!");
+ }
+ }
+
+function validarFormulario2() {
+  const nombre = document.getElementById('nombre').value;
+  const email = document.getElementById('email').value;
+  const mensaje = document.getElementById('mensaje');
+
+  if (nombre.length < 3) {
+    mensaje.innerText = "El nombre debe tener al menos 3 caracteres.";
+    mensaje.style.color = "red";
+      return false;
+
+  }
+  if (!email.includes('@') || !email.includes('.')) {
+    mensaje.innerText = "El email no es válido.";
+    mensaje.style.color = "red";
+
+    return false;
+  }
+  return true;
+}
